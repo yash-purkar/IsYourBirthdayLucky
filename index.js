c364e293dcbade845b4a6f896d27843474df0206
yashpurkar <yashpurkar7079@gmail.com>
2022-12-04 11:58:11 +0530

    got reference

---
+++ index.js
@@ -0,0 +1,8 @@
+const dateOfBirth = document.querySelector("#dob");
+const luckyNumer = document.querySelector("#luckyNum");
+const checkBtn = document.querySelector("#check_num");
+
+checkBtn.addEventListener("click", function check() {
+  console.log("clicked");
+})
+
