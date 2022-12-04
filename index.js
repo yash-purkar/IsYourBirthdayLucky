1ccc2eaefc34b74fa88053a2db517e5f312eb5d7
yashpurkar <yashpurkar7079@gmail.com>
2022-12-04 12:16:47 +0530

    got sum of the date

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const dateOfBirth = document.querySelector("#dob");
-const luckyNumer = document.querySelector("#luckyNum");
+const luckyNumber = document.querySelector("#luckyNum");
 const checkBtn = document.querySelector("#check_num");
 
-checkBtn.addEventListener("click", function check() {
-  console.log("clicked");
-})
+console.log(dateOfBirth.value);
+
+function checkBirthDateIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", ""); //It will remove all the hyphens
+  // console.log(typeof dob);
+  let sum = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sum = sum + Number(dob.charAt(i))
+  }
+  console.log(sum);
+  return sum;
+}
+
+checkBtn.addEventListener("click", checkBirthDateIsLucky)
 
